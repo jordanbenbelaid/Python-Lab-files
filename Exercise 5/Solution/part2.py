392d93c274d4f3aca72b98eb74eed297fe6ce8df
Jordan <jordanbenbelaid@hotmail.com>
2025-06-23 14:51:26 +0100

    removed ex 5

--- Exercise 5/Solution/part2.py
+++
@@ -1,4 +0,0 @@
-movie = {"title": "Jaws", "director": "Steven Spielberg", "year": 1975, "duration": 124.0}
-print("Title:" + movie["title"])
-print("Year:" + str(movie["year"]))
-print("Director:" + movie["director"])
